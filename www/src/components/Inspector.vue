1d6c03479edfd75acff03748c4b4fb3df3b4343d
Alf <alf.g.jr@gmail.com>
2020-09-01 22:41:50 -0700

    Add progress bar when loading file reader.

--- www/src/components/Inspector.vue
+++ www/src/components/Inspector.vue
@@ -12,6 +12,12 @@
           ></b-form-file>
         </b-form-group>
 
+        <b-progress
+          height="2px"
+          v-if="showProgress"
+          :value="progress"
+          max="100"></b-progress>
+
         <div v-if="data">
           <div class="mt-3">Selected file: {{ file ? `${file.name}: ${file.size} bytes` : '' }}</div>
 
@@ -47,6 +53,8 @@ export default {
     return {
       file: null,
       data: null,
+      progress: 0,
+      showProgress: false,
     }
   },
   computed: {
@@ -62,12 +70,20 @@ export default {
   },
   methods: {
     onFile(event) {
+      this.showProgress = true;
       const file = event.target.files[0];
       const reader = new FileReader();
 
       // reader.onload = e => this.$emit("load", event.target.result);
       reader.onload = (event) => {
+        this.progress = 100;
         this.data = new Uint8Array(event.target.result);
+        setTimeout(() => { this.showProgress = false; }, 2000);
+      }
+      reader.onprogress = (event) => {
+        if (event.lengthComputable) {
+          this.progress = parseInt(((event.loaded / event.total) * 100), 10);
+        }
       }
       reader.readAsArrayBuffer(file);
     }
